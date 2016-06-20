5281a851a76029c65634d173ff91933554beacc9
Kevin Cohen <99036164@mydavinci.nl>
2016-06-20 10:43:25 +0200

    2

--- index.logic.php
+++ index.logic.php
@@ -2,12 +2,19 @@
 	
 	$db = new mysqli('localhost','root','','calender');
 
-	$query = "SELECT * birthdays.person, birthdays.day, birthdays.month_id, birthdays.year, month.month, month.id As all LEFT JOIN month ON birthdays.month_id=month.id ORDER BY month_id, day";
+	$query = "SELECT birthdays.person, birthdays.day, birthdays.month_id, birthdays.year, month.id, month.month 
+	FROM birthdays
+	LEFT JOIN month 
+	ON birthdays.month_id=month.id
+	ORDER BY month_id, day";
 
 	$result = $db->query($query);
 	
 	$calender = $result->fetch_all(MYSQLI_ASSOC);
 
+	$currMonth_id = 'month_id';
+	$currDay = null;
+	$currMonth = null;
 		
 	
 ?>	
--- index.php
+++ index.php
@@ -11,40 +11,24 @@
 	
 	<body>
 
+
 <?php
 foreach ($calender as $calender_) :
 
-
-
-
-
-    if ($currMonth_id != $calender_ ['all']) {
-                    $currMonth = $calender_['all'];
+    if ($currMonth_id != $calender_ ['month']) {
+                    $currMonth = $calender_['month_id'];
                     echo "<h1>" . $calender_['month'] . "</h1>";
                 }
                 if ($currDay != $calender_['day']) {
                     $currDay = $calender_['day'];
                     echo"<h2>" . $calender_['day'] . "</h2>";
+                    echo $calender_['person'];
                 } ?>
-
-
-
-
-
-
-
-
 
 <?php
     endforeach;
 ?>
-    </tbody>
- </table>
-
-
-
-
-
+ 
 
     <p><a href="create.php">+ Toevoegen</a></p>
 
